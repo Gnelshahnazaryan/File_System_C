6ce63c38bbb2eca57517535c51ebad1376eaaeb1
Gnelshahnazaryan <gshahnazaryan08@gmail.com>
2025-09-07 21:01:48 +0400

    Update

--- struct_in_file.c
+++ struct_in_file.c
@@ -1,70 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct Person{
 
+	int age;
 	char name[50];
 	char surname[50];
 
 
 }Person;
 
-int main(){
+void PersonToFile(Person* obj){
 
-	FILE* file = fopen("./StructFile.txt", "w");
+	FILE* file = fopen("./struct.bin", "ab");
 
 	if(file == NULL){
 
 		perror("Error opening file");
-		return 0;
+		return;
 
 	}
 
-	Person P1;
-
 	printf("Enter name,surname and age: ");
-	scanf("%s %s",P1.name,P1.surname);
+	scanf("%s %s %d",obj->name,obj->surname,&obj->age);
 
+	size_t written = fwrite(obj, sizeof(Person), 1, file);
 
-	int person = fprintf(file,"%s\n","Person 1");
+        if (written == 1) {
 
-	int fpf = fprintf(file,"Name: %s ",P1.name);
-	int fpf1 = fprintf(file,"Surname: %s\n",P1.surname);
+            printf("Struct written successfully.\n");
 
-	if(fpf == 0 || fpf1 == 0){
+        } else {
 
-		return 0;
+            printf("Write error occurred.\n");
+
+        }
+
+	fclose(file);
+
+}
+
+void Print_Person(Person* obj){
+
+	 FILE* file = fopen("./struct.bin","rb");
+
+	 if(file == NULL){
+
+		perror("Error opening file");
+		return;
+
+	 } 
+
+	 size_t read = fread(obj, sizeof(Person), 1, file);
+
+        if (read == 1) {
+
+            printf("Name: %s\n Surname: %s\n Age: %d\n", obj->name,obj->surname,obj->age);
+
+        } else {
+
+            printf("Error reading the file or reached EOF.\n");
+
+        }
+
+	fclose(file);
+
+}
+
+void restore(Person obj,int num){
+
+	FILE* file = fopen("./struct.bin","rb");
+
+	if(file == NULL){
+
+		perror("Can't open file;");
+		return;
+
+	}
+
+	fseek(file, 0, SEEK_SET);
+	fseek(file,num * sizeof(Person),SEEK_SET);
+	
+	size_t read = fread(&obj, sizeof(Person), 1, file);
+	 
+	if(read == 1){
+
+    	printf("Name: %s Surname: %s Age: %d\n", obj.name,obj.surname,obj.age);
+
+	}else{
+
+    	printf("Error reading the file or reached EOF.\n");
 
 	}
 
 	fclose(file);
 
-
-	FILE* file1 = fopen("./StructFile.txt", "r");
-
-	char name[50];
-	char surname[50];
-	
-	
-	while(fgets(name,sizeof(name),file1)){
-
-		printf("%s",name);
-
-	}
-
-	 fseek(file1, sizeof(name), SEEK_SET);
+}
 
 
-	while(fgets(surname,sizeof(surname),file1)){
+int main(){
 
-		printf("%s",surname);
+	Person p;
+
+	for(int i = 0; i < 3; i++){
+
+		PersonToFile(&p);
 
 	}
 
-
-	fclose(file1);
-	return 0;
+	restore(p,1);
 
 
+	return 0;	
 }
